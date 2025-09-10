a5bcd8b0909c92be503bf56dbf00e70483d229f3
Andrew Le <a.asterisk.le@gmail.com>
2025-09-10 13:06:58 -0500

    Added necessary DAO and Service files for objects. Added Winston Logger for debugging and logging.

---
+++ src/util/logger.js
@@ -0,0 +1,26 @@
+const { createLogger, transports, format } = require("winston");
+
+const logger = createLogger({
+  format: format.combine(
+    format.timestamp(),
+    format.printf(({ timestamp, level, message }) => {
+      return `${timestamp} [${level}]: ${message}`;
+    })
+  ),
+  transports: [
+    new transports.Console(),
+    new transports.File({ filename: "app.log", level: "info"}),
+    new transports.File({ filename: "error.log", level: "error"}) 
+  ],
+});
+
+
+function loggerMiddleware(req, res, next){
+    logger.info(`Incoming ${req.method} : ${req.url}`);
+    next();
+}
+
+module.exports = {
+  logger,
+  loggerMiddleware
+}
